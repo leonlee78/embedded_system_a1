9b92527806215507d568e7dc5d936a36b046b203
leonlee78 <breakingcwalker@gmail.com>
2021-02-05 22:21:05 -0500

    Add code outline

--- stats.h
+++ stats.h
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h 
+ * @brief Header file 
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Leon Lee
+ * @date 2/5/2021
  *
  */
 #ifndef __STATS_H__
@@ -36,5 +36,77 @@
  * @return <Add Return Informaiton here>
  */
 
+/**
+ * @brief print the statistics of the given array in console
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return void
+ */
+void print_statistics(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief print the elements in the array in console
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return void
+ */
+void print_array(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief Find the median of the array
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return the median
+ */
+unsigned char find_median(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief Find the mean of the array
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return the mean
+ */
+unsigned char find_mean(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief Find the max of the array
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return the max
+ */
+unsigned char find_maximum(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief Find the min of the array
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return the min
+ */
+unsigned char find_minimum(unsigned char* arr, unsigned int a_size);
+/**
+ * @brief swap two values
+ *
+ * @param xp pointer to first element
+ * @param xy pointer to second element
+ *
+ * @return void
+ */
+void swap(unsigned char* xp, unsigned char* yp);
+/**
+ * @brief Sort the array in descending order
+ *
+ * @param arr pointer to array
+ * @param a_size array size
+ *
+ * @return void
+ */
+void sort_array(unsigned char* arr, unsigned int a_size);
 
 #endif /* __STATS_H__ */
