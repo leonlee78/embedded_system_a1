4082bdb87e776822120ba54caafc7ad353469e77
leonlee78 <breakingcwalker@gmail.com>
2021-02-05 22:24:50 -0500

    Implement the functions

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
+ * @file stats.c 
  * @brief <Add Brief Description Here >
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Leon Li
+ * @date 2/5/2021
  *
  */
 
@@ -35,9 +35,107 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+  print_array(test, SIZE);
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
 }
 
+void print_statistics(unsigned char* arr, unsigned int a_size)
+{
+  printf("The maximum of the array is %d\n", find_maximum(arr, a_size));
+  printf("The minimum of the array is %d\n", find_minimum(arr, a_size));
+  printf("The mean of the array is %d\n", find_mean(arr, a_size));
+  printf("The median of the array is %d\n", find_median(arr, a_size));
+}
+
+void print_array(unsigned char* arr, unsigned int a_size)
+{
+  printf("Printing array:\n");
+  for (unsigned int idx = 0; idx < a_size; idx++)
+  {
+    printf("%d ",arr[idx]);
+  }
+  printf("\nFinished!\n");
+}
+
+unsigned char find_median(unsigned char* arr, unsigned int a_size)
+{
+  sort_array(arr, a_size);
+  if (a_size%2 == 0)
+  {
+    return (arr[a_size/2] + arr[a_size/2 + 1]) / 2;
+  }
+  else
+  {
+    return arr[a_size/2];
+  }
+}
+
+unsigned char find_mean(unsigned char* arr, unsigned int a_size)
+{
+  unsigned int sum = 0;
+  for (unsigned int idx = 0; idx < a_size; idx++)
+  {
+    sum += arr[idx];
+  }
+  unsigned char result = sum / a_size;
+  return result;
+}
+
+unsigned char find_maximum(unsigned char* arr, unsigned int a_size)
+{
+  unsigned char result = 0;
+  for (unsigned int idx = 0; idx < a_size; idx++)
+  {
+    if (arr[idx] > result)
+    {
+      result = arr[idx];
+    }
+  }
+  return result;
+}
+
+unsigned char find_minimum(unsigned char* arr, unsigned int a_size)
+{
+  unsigned char result = 255;
+  for (unsigned int idx = 0; idx < a_size; idx++)
+  {
+    if (arr[idx] < result)
+    {
+      result = arr[idx];
+    }
+  }
+  return result;
+}
+
+void swap(unsigned char* xp, unsigned char* yp) 
+{ 
+    unsigned char temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+
+void sort_array(unsigned char* arr, unsigned int a_size)
+{
+  printf("Sorting Array\n");
+  unsigned int i, j, max_idx; 
+
+  // One by one move boundary of unsorted subarray 
+  for (i = 0; i < a_size - 1; i++) { 
+
+      // Find the maximum element in unsorted array 
+      max_idx = i; 
+      for (j = i + 1; j < a_size; j++) 
+          if (arr[j] > arr[max_idx]) 
+              max_idx = j; 
+
+      // Swap the found maximum element 
+      // with the first element 
+      swap(&arr[max_idx], &arr[i]); 
+  } 
+}
 /* Add other Implementation File Code Here */
